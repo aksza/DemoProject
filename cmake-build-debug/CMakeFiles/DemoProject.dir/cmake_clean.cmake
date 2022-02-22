4890df041499bdd7c46e03e4735cd3c2c4899fdf
Aksza <suciu.aksza@student.ms.sapientia.ro>
2022-02-22 20:08:15 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DemoProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoProject.dir/main.c.obj"
+  "DemoProject.exe"
+  "DemoProject.exe.manifest"
+  "DemoProject.pdb"
+  "libDemoProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DemoProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
